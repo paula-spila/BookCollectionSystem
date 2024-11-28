ae00818bf5860b562e736be1ffad2a8668968527
paula-spila <paula.spila@gmail.com>
2024-11-28 22:27:32 +0200

    book cards and filters fixed

--- force-app/main/default/lwc/bookCard/bookCard.js
+++ force-app/main/default/lwc/bookCard/bookCard.js
@@ -1,20 +1,49 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getBooks from '@salesforce/apex/BookInUserCollectionController.getBooks';
 
-export default class BookCard extends LightningElement {
+export default class BookCard extends NavigationMixin(LightningElement) {
   @api pageName;
+  @api filters = {};
+  @track books;
+  @track filteredBooks;
+  @track isLoading = true;
 
-  // @api book;
+  @wire(getBooks, { collectionType: '$pageName' })
+  wiredBooks({ error, data }) {
+    if (data) {
+      this.books = data.filter(bookEntry => bookEntry.Book__r);
+      this.applyFilters(this.filters);
+      this.isLoading = false;
+    } else if (error) {
+      this.isLoading = false;
+    }
+  }
 
-  // handleLendBook() {
-  //   this.dispatchEvent(new CustomEvent('lendbook', { detail: this.book.Id }));
-  // }
+  @api
+  applyFilters(filters = {}) {
+    if (!this.books) {
+      return;
+    }
 
-  // handleAddBookToCollection() {
-  //   this.dispatchEvent(new CustomEvent('addbooktocollection', { detail: this.book.Id }));
-  // }
+    this.filteredBooks = this.books.filter(bookEntry => {
+      const titleMatch = !filters.title || bookEntry.Book__r.Name.toLowerCase().includes(filters.title.toLowerCase());
+      const genreMatch = !filters.genre || (bookEntry.Book__r.Genre__r && bookEntry.Book__r.Genre__r.Name === filters.genre);
+      const authorMatch = !filters.author || `${bookEntry.Book__r.Author__r.Name} ${bookEntry.Book__r.Author__r.Surname__c}`.toLowerCase().includes(filters.author.toLowerCase());
+      return titleMatch && genreMatch && authorMatch;
+    });
+  }
 
-  // handleViewDetails() {
-  //   this.dispatchEvent(new CustomEvent('viewdetails', { detail: this.book.Id }));
-  // }
 
+  handleCardClick(event) {
+    const bookInUserCollectionId = event.currentTarget.dataset.id;
+    this[NavigationMixin.Navigate]({
+      type: 'standard__recordPage',
+      attributes: {
+        recordId: bookInUserCollectionId,
+        objectApiName: 'BookInUserCollection__c',
+        actionName: 'view',
+      },
+    });
+  }
 }
